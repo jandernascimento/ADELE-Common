b9a68d248b73e66e9ce52035f803b10bad81b955
Thomas Leveque <leveque.thomas@gmail.com>
2012-12-08 20:08:35 +0100

    move packages and distribution from M2MAppBuilder

---
+++ distribution/base/debug.bat
@@ -0,0 +1,2 @@
+cd /Users/thomas/workspaces/M2MAppBuilder/distribution/base/target/distrib-temp
+java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=y -jar bin\felix.jar
---
+++ distribution/base/start.bat
@@ -0,0 +1,2 @@
+cd /Users/thomas/workspaces/M2MAppBuilder/distribution/base/target/distrib-temp
+java -jar bin\felix.jar
