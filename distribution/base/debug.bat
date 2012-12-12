d5a76cbf2abbe147d02a8330124c6f57a5891b58
torito <torito@gmail.com>
2012-12-12 17:22:27 +0100

    change fileinstall to use 3.1.4 in parent and modify script in minimal distribution

--- distribution/base/debug.bat
+++
@@ -1,2 +0,0 @@
-cd /Users/thomas/workspaces/ADELE-Common/distribution/base/target/distrib-temp
-java -Xdebug -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=y -jar bin\felix.jar
--- distribution/base/start.bat
+++
@@ -1,2 +0,0 @@
-cd /Users/thomas/workspaces/ADELE-Common/distribution/base/target/distrib-temp
-java -jar bin\felix.jar
