a9dcf6028d881422b98248558561879c775a0a99
Thomas Leveque <leveque.thomas@gmail.com>
2013-02-19 14:55:09 +0100

    fix discovering of current directory on Windows when starting IHM

--- distribution/config-base/src/main/resources/startIHM.bat
+++ distribution/config-base/src/main/resources/startIHM.bat
@@ -1,3 +1,3 @@
-for %%F in ("%filename%") do set "dirName=%%~dp"
-ECHO %dirName%
-java -DapplyEvolutions.default=true -cp "%dirName%\bin\*;%dirName%\lib\*" play.core.server.NettyServer "%dirName%"
+set dirname=%CD%
+ECHO %dirname%
+java -DapplyEvolutions.default=true -cp "%dirname%\bin\*;%dirname%\lib\*" play.core.server.NettyServer "%dirname%"
