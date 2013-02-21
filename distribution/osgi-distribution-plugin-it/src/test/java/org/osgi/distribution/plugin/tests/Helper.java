dce7a8ee8048762ab3ffa01ba3a9231a92bafae4
hitogore <mehdi1.kettani@orange.com>
2013-02-21 17:56:11 +0100

    initial commit of osgi distrib plugin it tests.

---
+++ distribution/osgi-distribution-plugin-it/src/test/java/org/osgi/distribution/plugin/tests/Helper.java
@@ -0,0 +1,8 @@
+package org.osgi.distribution.plugin.tests;
+
+public class Helper {
+
+	public static final String TEST_GROUP_ID = "org.osgi.distribution.plugin.tests";
+	public static final String TEST_VERSION = "0.0.1-SNAPSHOT";
+
+}
