a8297f1b4bad0eb7e0226b64dd805bee30ae9335
torito <torito@gmail.com>
2013-03-13 14:33:13 +0100

    Add stability check instead of arbitrary delay

--- tests/distribution-tests/distribution-test-base/src/test/java/fr/liglab/adele/icasa/distribution/test/DistributionBaseTest.java
+++ tests/distribution-tests/distribution-test-base/src/test/java/fr/liglab/adele/icasa/distribution/test/DistributionBaseTest.java
@@ -1,11 +1,11 @@
 
 package fr.liglab.adele.icasa.distribution.test;
  
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.options;
+
 import javax.inject.Inject;
 
-
-import static org.junit.Assert.*;
-import static org.ops4j.pax.exam.CoreOptions.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
@@ -15,6 +15,7 @@ import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerMethod;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
   
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerMethod.class)
@@ -33,24 +34,82 @@ public class DistributionBaseTest {
  
     @Test
     public void testBundlesValidity() {
-    	waitSomeTime(10000);
-    	Bundle[] bundles = context.getBundles();
-    	for (Bundle bundle: bundles){
-    		System.out.println("bundle:" + bundle.getSymbolicName());
-    		System.out.println("state:" + bundle.getState());
-    		assertNotSame(Bundle.INSTALLED, bundle.getState());
-    		assertNotSame(Bundle.STOP_TRANSIENT, bundle.getState());
-    		assertNotSame(Bundle.STOPPING, bundle.getState());
-    		assertNotSame(Bundle.UNINSTALLED, bundle.getState());
-    	}
-        
+    	waitForStability(context);
     }
-    
-	public static void waitSomeTime(int l) {
-		try {
-			Thread.sleep(l);//wait to be registered
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+
+    /**
+     * Waits for stability:
+     * <ul>
+     * <li>all bundles are activated
+     * <li>service count is stable
+     * </ul>
+     * If the stability can't be reached after a specified time,
+     * the method throws a {@link IllegalStateException}.
+     * @param context the bundle context
+     * @throws IllegalStateException when the stability can't be reach after a several attempts.
+     */
+    private void waitForStability(BundleContext context) throws IllegalStateException {
+        // Wait for bundle initialization.
+        boolean bundleStability = getBundleStability(context);
+        int count = 0;
+        while (!bundleStability && count < 500) {
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                // Interrupted
+            }
+            count++;
+            bundleStability = getBundleStability(context);
+        }
+
+        if (count == 500) {
+            System.err.println("Bundle stability isn't reached after 500 tries");
+            throw new IllegalStateException("Cannot reach the bundle stability");
+        }
+
+        boolean serviceStability = false;
+        count = 0;
+        int count1 = 0;
+        int count2 = 0;
+        while (! serviceStability && count < 500) {
+            try {
+                ServiceReference[] refs = context.getServiceReferences((String) null, null);
+                count1 = refs.length;
+                Thread.sleep(500);
+                refs = context.getServiceReferences((String) null, null);
+                count2 = refs.length;
+                serviceStability = count1 == count2;
+            } catch (Exception e) {
+                System.err.println(e);
+                serviceStability = false;
+                // Nothing to do, while recheck the condition
+            }
+            count++;
+        }
+
+        if (count == 500) {
+            System.err.println("Service stability isn't reached after 500 tries (" + count1 + " != " + count2);
+            throw new IllegalStateException("Cannot reach the service stability");
+        }
+    }
+
+    /**
+     * Are bundle stables.
+     * @param bc the bundle context
+     * @return <code>true</code> if every bundles are activated.
+     */
+    private boolean getBundleStability(BundleContext bc) {
+        boolean stability = true;
+        Bundle[] bundles = bc.getBundles();
+        for (int i = 0; i < bundles.length; i++) {
+        	int state = bundles[i].getState();
+            stability = stability && ((state == Bundle.ACTIVE) || (state == Bundle.RESOLVED));
+            if (!((state == Bundle.ACTIVE) || (state == Bundle.RESOLVED))){
+            	System.err.println("Waiting to stability for: " + bundles[i].getSymbolicName());
+            } else {
+            	System.err.println("Correctly installed: " + bundles[i].getSymbolicName());
+            }
+        }
+        return stability;
+    }
 }
